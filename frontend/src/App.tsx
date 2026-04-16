1dae0b9345638142f48a9cb0f2e24ff33d3b2289
Flor Stellamans <florstellamamans@gmail.com>
2026-04-16 16:42:30 +0200

    Create add and remove client order

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,29 @@ import type {ClientOrder, LabelOrder} from "./types/label.ts";
 import FolieSettings from "./components/FolieSettings.tsx";
 import LabelList from "./components/LabelList.tsx";
 
+let nextId = 0;
 
 export default function App() {
-
     const [clientOrder, setClientOrder] = useState<ClientOrder>({
         foilLength: 300,
         labelOrders: []
     })
 
     function addClientOrder() {
-        // add new labelOrder in labelOrders list
+        const newOrder: LabelOrder = {
+            id: nextId,
+            labelRollsOrdered: 1,
+            label: {
+                articleNumber: "",
+                height: 0,
+                width: 0,
+                gap: 0,
+                labelsPerRow: 1,
+                totalLabels: 0,
+            }
+        }
+        setClientOrder({...clientOrder, labelOrders: [...clientOrder.labelOrders, newOrder]})
+        nextId += 1;
     }
 
     function updateLabelValue(param: string, value: number | string) {
@@ -20,7 +33,8 @@ export default function App() {
     }
 
     function removeLabelOrder(labelOrder: LabelOrder) {
-        // remove the label order from the label orders list
+        const newOrderList = clientOrder.labelOrders.filter(l => l.id !== labelOrder.id)
+        setClientOrder({...clientOrder, labelOrders: newOrderList})
     }
 
     return (
